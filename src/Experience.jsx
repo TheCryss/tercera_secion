52b14355d046bc16f54eda2d4ca45920445cbe7b
Jose Luis <jose.bucheli@correounivalle.edu.co>
2023-09-06 10:06:11 -0500

    tree added

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,5 @@
 import Bee from "./World/Bee";
+import Tree from "./World/Tree";
 
 const Experience = () => {
     return (
@@ -6,6 +7,7 @@ const Experience = () => {
             <ambientLight intensity={0.5} />
             <directionalLight position={[10, 10, 5]} intensity={2} />
             <Bee></Bee>
+            <Tree position={[4, -11, 0]} scale={0.1}  />
         </>
     )
 }
