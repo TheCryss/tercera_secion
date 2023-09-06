585cd8f3b91fed8a8fdf2bb886f9dc915fbbf455
Jose Luis <jose.bucheli@correounivalle.edu.co>
2023-09-06 09:47:45 -0500

    bee propieties

--- src/World/Bee.jsx
+++ src/World/Bee.jsx
@@ -3,7 +3,11 @@ const { useGLTF } = require("@react-three/drei")
 const Bee = () => {
     const beeModel = useGLTF('/assets/models/bee/scene.gltf')
     console.log(beeModel);
-    return
+    return(
+      <mesh mesh position-x={2} rotation-y={-Math.PI * 0.15} scale={2}>
+            <primitive object={beeModel.scene} />
+      </mesh>
+    )
 }
 export default Bee;
 useGLTF.preload("/assets/models/bee/scene.gltf");
