a803b9db4ab69aae8235c5421d5594acca6a2c14
Jose Luis <jose.bucheli@correounivalle.edu.co>
2023-09-06 09:32:05 -0500

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Canvas } from "@react-three/fiber"
+import Experience from "./Experience"
+import Info from "./Info/Info"
+
+const App = () => {
+    // Camera settings
+    const cameraSettings = {
+        position: [0, 0, 5],
+        fov: 60
+    }
+
+    return (
+        <>
+            <Info name={"Fabian Valencia"} biography={"Web 3D Developer"} />
+            <Canvas
+                camera={cameraSettings}
+            >
+                <Experience />
+            </Canvas>
+        </>
+    )
+}
+export default App
