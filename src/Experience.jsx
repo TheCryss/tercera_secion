fcd3c1df3092973c36149bfb9197af5ae86ed09b
Jose Luis <jose.bucheli@correounivalle.edu.co>
2023-09-06 09:39:03 -0500

    bee assets

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,10 +1,11 @@
-
+import Bee from "./World/Bee";
 
 const Experience = () => {
     return (
         <>
             <ambientLight intensity={0.5} />
             <directionalLight position={[10, 10, 5]} intensity={2} />
+            <Bee></Bee>
         </>
     )
 }
