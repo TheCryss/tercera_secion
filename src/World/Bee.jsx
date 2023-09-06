df740872951a1d45c899b972dc60b4170d277ce2
Jose Luis <jose.bucheli@correounivalle.edu.co>
2023-09-06 10:26:38 -0500

    bee animations done

--- src/World/Bee.jsx
+++ src/World/Bee.jsx
@@ -1,10 +1,20 @@
-const { useGLTF } = require("@react-three/drei")
+import { useEffect, useRef } from "react";
+
+const { useAnimations,useGLTF } = require("@react-three/drei")
+
 
 const Bee = () => {
     const beeModel = useGLTF('/assets/models/bee/scene.gltf')
     console.log(beeModel);
+    const beeRef =useRef();
+    const {animations} = beeModel;
+    const { actions } = useAnimations(animations, beeRef);
+    useEffect(() => {
+        const action = actions["All Animations"]
+        action.play()
+    }, [])
     return(
-      <mesh mesh position-x={2} rotation-y={-Math.PI * 0.15} scale={2}>
+      <mesh ref={beeRef} mesh position-x={2} rotation-y={-Math.PI * 0.15} scale={2}>
             <primitive object={beeModel.scene} />
       </mesh>
     )
